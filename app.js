2db8f833d87719160f02e1e35666e6538c214b87
Joseph Method <tristil@gmail.com>
2012-04-25 19:21:24 +0000

    Playing with socket.io

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
+var io = require('socket.io').listen(app);
+
 // Configuration
 
 app.configure(function(){
@@ -28,9 +30,9 @@ app.configure('production', function(){
 });
 
 // Routes
-app.get('/', routes.index);
-app.post('/', routes.index);
+routes.index(app, io);
 
 app.listen(8081, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,63 @@
-var Person  = require('../models/person').Person;
+exports.index = function(app, io)
+{
+  var self = this;
+  self.socket = null;
+  self.app = app;
+  self.io = io;
+  var Person  = require('../models/person').Person;
+  self.person_model = new Person();
 
-/*
- * GET home page.
- */
+  self.io.sockets.on('connection', function(socket) {
+    console.log('Welcome!');
+    self.socket = socket;
+    self.socket.on('hello', function()
+      {
+        console.log('Oh, hello');
+      }
+    );
+  });
 
-exports.index = function(req, res){
-  this.person_model  = new Person();
-
-  this.display_people = function()
+  this.view = function(req, res)
   {
+    self.person_model  = new Person();
     var people = [];
 
-    this.person_model.findAll(
+    self.person_model.findAll(
         function(err, people)
         {
           res.render('index', { title: 'MotiveWorld', people : people});
         }
-    );
+        );
+
   };
 
-  if(req.body.name)
+  this.post = function(req, res)
   {
-    var person = this.person_model.create();
-    person.setData({name : req.body.name});
-    person.save(
-        function(err, next_id)
-        {
-          this.display_people();
-        }
-    );
-  }
-  else
-  {
-    this.display_people();
-  }
+    if(req.body.name)
+    {
+      var person = self.person_model.create();
+      person.setData({name : req.body.name});
+      person.save(
+          function(err, next_id)
+          {
+            console.log('Person added');
+            if(self.socket)
+            {
+              self.socket.emit('person-added', {id : next_id, name : req.body.name });
+            }
+            res.send('1');
+            return;
+          }
+          );
+    }
+    else
+    {
+      res.send('0');
+    }
+  };
 
+  app.get('/', this.view);
+  app.post('/', this.post);
 };
+
+
